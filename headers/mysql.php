d3b02707eebe8386bf8136d43a6a139bb3fdc45c
MattBubernak <soccerfreak24@gmail.com>
2014-03-31 19:03:38 -0600

    fixed deprecated mysql connudction

--- headers/mysql.php
+++ headers/mysql.php
@@ -1,8 +1,16 @@
 <?php
 //$con = mysql_connect("mattkgrosscom.ipagemysql.com", "thedonald", "footlong");
 // make sure to set the default_host in php.ini to mattkgrosscom.ipagemysql.com
-$con = mysql_connect(ini_get("mysql.default_host"), "cockfight", "SouthDakota3Way");
-if(!$con)
+$con = mysqli_init(); 
+
+if (!$con) 
+{
+	die("mysqli_init failed"); 
+}
+
+$connection = mysqli_real_connect($con, ini_get("mysql.default_host"), "root", "");
+
+if(!$connection)
 	die("Database connection failure! Please alert webmaster.");
-mysql_select_db("workout_beta");
+mysql_select_db("workout");
 ?>
