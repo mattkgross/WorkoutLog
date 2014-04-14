1937ce17698a10c262d7bdf640888a523f517fe2
Matt Gross <mattkgross@gmail.com>
2014-04-13 21:12:32 -0600

    attempted to set up for azure db

--- headers/mysql.php
+++ headers/mysql.php
@@ -2,9 +2,37 @@
 // Hide deprecation errors - switch to mysqli for future apps, though
 error_reporting(E_ALL ^ E_DEPRECATED);
 
-//$con = mysql_connect("mattkgrosscom.ipagemysql.com", "thedonald", "footlong");
-// make sure to set the default_host in php.ini to mattkgrosscom.ipagemysql.com
-$con = mysql_connect(ini_get("mysql.default_host"), "root", "");
+function connStrToArray($conn_str){
+ 
+    // Initialize array.
+    $conn_array = array();
+ 
+    // Split conn string on semicolons. Results in array of "parts".
+    $parts = explode(";", $conn_str);
+ 
+ 
+    // Loop through array of parts. (Each part is a string.)
+    foreach($parts as $part){
+ 
+        // Separate each string on equals sign. Results in array of 2 items.
+        $temp = explode("=", $part);
+ 
+        // Make items key=>value pairs in returned array.
+        $conn_array[$temp[0]] = $temp[1];
+    }
+ 
+    return $conn_array;
+}
+
+$conn_str = getenv("MYSQLCONNSTR_DefaultConnection");
+
+if(!empty($conn_str)) {
+	$conn_array = connStrToArray($conn_str);
+	$con = mysql_connect($conn_array['Data Source'], $conn_array['User Id'], $conn_array['Password']);
+}
+else {
+	$con = mysql_connect(ini_get("mysql.default_host"), "root", "");
+}
 if(!$con)
 	die("Database connection failure! Please alert webmaster.");
 mysql_select_db("workout");
