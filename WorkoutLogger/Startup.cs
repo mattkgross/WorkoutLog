7ed8b01ac372c6a0c523112c094c984712cafb92
Matt Gross <mattkgross@gmail.com>
2016-03-15 14:40:30 -0600

    MVC Template

---
+++ WorkoutLogger/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorkoutLogger.Startup))]
+namespace WorkoutLogger
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
