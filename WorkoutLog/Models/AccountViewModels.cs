cfc1973d2f19dcd9bb949300d17c3e7fb5d9a843
Matt Gross <mattkgross@gmail.com>
2016-03-16 17:22:31 -0600

    Added maxlengths for registration & empty db
    Still need to figure out how to configure app to use said db...

--- WorkoutLog/Models/AccountViewModels.cs
+++ WorkoutLog/Models/AccountViewModels.cs
@@ -65,22 +65,25 @@ namespace WorkoutLog.Models
     public class RegisterViewModel
     {
         [Required]
+        [MaxLength(50, ErrorMessage = "Maximum of 50 characters allowed for your first name.")]
         [DataType(DataType.Text)]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Required]
+        [MaxLength(50, ErrorMessage = "Maximum of 50 characters allowed for your last name.")]
         [DataType(DataType.Text)]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
         [Required]
+        [MaxLength(50, ErrorMessage = "Maximum of 50 characters allowed for your email.")]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 12)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 10)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
