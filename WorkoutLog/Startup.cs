2932a45c348118e0a8c2d369eedaa675374ac873
Matt Gross <mattkgross@gmail.com>
2016-04-23 22:09:46 -0600

    Web Form Stub
    In all of its glory.

---
+++ WorkoutLog/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorkoutLog.Startup))]
+namespace WorkoutLog
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
