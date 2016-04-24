83ea4c933f6b4b3ca80499613216661e2832b2e1
Matt Gross <mattkgross@gmail.com>
2016-04-24 16:14:33 -0600

    Installed SendGrid for future email purposes

--- WorkoutLog/App_Start/IdentityConfig.cs
+++ WorkoutLog/App_Start/IdentityConfig.cs
@@ -39,14 +39,14 @@ namespace WorkoutLog
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
         {
             var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
-            // Configure validation logic for usernames
+            // Configure validation logic for usernames.
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
                 AllowOnlyAlphanumericUserNames = true,
                 RequireUniqueEmail = true
             };
 
-            // Configure validation logic for passwords
+            // Configure validation logic for passwords.
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 10,
@@ -56,7 +56,7 @@ namespace WorkoutLog
                 RequireUppercase = true,
             };
 
-            // Configure user lockout defaults (retrieve them from Web.config)
+            // Configure user lockout defaults.
             manager.UserLockoutEnabledByDefault = true;
             manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(15);
             manager.MaxFailedAccessAttemptsBeforeLockout = 7;
