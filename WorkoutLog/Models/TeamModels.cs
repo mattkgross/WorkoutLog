f0b6bb228f845550d67fec08f5e00b1e89536a01
Matt Gross <mattkgross@gmail.com>
2016-04-24 23:13:14 -0600

    Team stub.
    Added model and web form.

---
+++ WorkoutLog/Models/TeamModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutLog.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string EnrollmentKey { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+
+    public class TeamDBContext : DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+    }
+}
