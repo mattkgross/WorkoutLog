a17121f1698b6419a2fb01292385808b0ad04d9d
Matt Gross <mattkgross@gmail.com>
2016-06-23 22:40:47 -0600

    CSS + Nuget
    Now just need to re-add page content w/ classes

--- WorkoutLog/App_Start/BundleConfig.cs
+++ WorkoutLog/App_Start/BundleConfig.cs
@@ -23,9 +23,11 @@ namespace WorkoutLog
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").IncludeDirectory(
+                      "~/Content", "*.css"));
+
+            bundles.Add(new ScriptBundle("~/Content/purecss").IncludeDirectory(
+                      "~/Content/purecss", "*.css"));
         }
     }
 }
